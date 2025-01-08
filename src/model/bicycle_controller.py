51595ed35ac4fc4be49b8f1c50abf71669691121
Tim Sandner <tim-sandner@gmx.de>
2025-01-08 16:13:44 +0100

    Added controller implementations

---
+++ src/model/bicycle_controller.py
@@ -0,0 +1,54 @@
+from abc import abstractmethod, ABC
+from dataclasses import dataclass
+
+from src.model.state import BicycleState
+
+
+@dataclass
+class BicycleControl:
+    roll_torque: float
+    steer_torque: float
+
+    def get_roll_torque(self):
+        return self.roll_torque
+
+    def get_steer_torque(self):
+        return self.steer_torque
+
+
+
+class BicycleController(ABC):
+    @abstractmethod
+    def calculate_control(self, state: BicycleState) -> BicycleControl:
+        pass
+
+    @abstractmethod
+    def get_parameters(self) -> dict[str, str]:
+        pass
+
+    def get_name(self) -> str:
+        return self.__class__.__name__
+
+class NoControlController(BicycleController):
+    """
+    Controller which applies no control input.
+    """
+    def calculate_control(self, state: BicycleState) -> BicycleControl:
+        return BicycleControl(0.0, 0.0)
+
+    def get_parameters(self) -> dict[str, str]:
+        return {}
+
+
+class RollRateFeedbackController(BicycleController):
+    """
+    Controller which applies a steer torque proportional to the negative roll angular rate
+    """
+    def __init__(self, gain: float):
+        self.gain = gain
+
+    def calculate_control(self, state: BicycleState) -> BicycleControl:
+        return BicycleControl(0.0, -self.gain * state.get_roll_rate())
+
+    def get_parameters(self) -> dict[str, str]:
+        return {'gain': str(self.gain)}
