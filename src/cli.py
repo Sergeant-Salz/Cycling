1638c8d7d78db60a156e35e4e7f58a7ca96e24a0
Tim Sandner <tim-sandner@gmx.de>
2025-01-09 15:52:27 +0100

    Added option to visualize instead of write to disk

--- src/cli.py
+++ src/cli.py
@@ -24,6 +24,9 @@ def setup_parser() -> argparse.ArgumentParser:
 
     # add a subparser for the simulate command
     simulate_parser = subparsers.add_parser('simulate', help='Simulate the bicycle model')
+    simulate_parser.add_argument('--show',
+                                 action='store_true',
+                                 help='Show the visualization instead of writing to a file')
     simulate_parser.add_argument('--output', '-o',
                                  type=Path,
                                  help='Output file for simulation results',
@@ -32,16 +35,32 @@ def setup_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def visualize(input_file: Path, verbose = False):
+def visualize(sim_res: SimulationResult, verbose = False):
+    visualize_animation(sim_res)
+
+def visualize_from_file(input_file: Path, verbose = False):
     if verbose:
         print(f'Loading {input_file} for visualization')
     # load the input file
     animation = SimulationResult.load_from(input_file)
-    # visualize the animation
-    visualize_animation(animation)
+    visualize(animation, verbose)
 
 
-def simulate(output_file: Path, verbose = False):
+def simulate(verbose = False) -> SimulationResult:
+    if verbose:
+        print('Simulating bicycle model')
+    # create initial state
+    initial_state = BicycleState(math.radians(5), math.radians(-2), 0, 0, 0, 0)
+    # create default parameters
+    parameters = SimulationParameters(initial_state=initial_state)
+    # create a simulation
+    simulation = Simulation(parameters)
+    # run the simulation
+    simulation.run()
+    # get the results
+    return simulation.get_result()
+
+def simulate_to_file(output_file: Path, verbose = False):
     if verbose:
         print(f'Simulating bicycle model and saving results to {output_file}')
     # check, that the output directory exists
@@ -50,18 +69,7 @@ def simulate(output_file: Path, verbose = False):
             print(f"Creating directory {output_file.parent.absolute()}")
         output_file.parent.mkdir(parents=True, exist_ok=True)
 
-    # create initial state
-    initial_state = BicycleState(math.radians(10), math.radians(-5), 0, 0, 0, 0)
-    # create default parameters
-    parameters = SimulationParameters(initial_state=initial_state)
-    # create a simulation
-    simulation = Simulation(parameters)
-    # run the simulation
-    simulation.run()
-    # get the results
-    results = simulation.get_result()
-    # save the results
-    results.save_to(output_file)
+    simulate(verbose).save_to(output_file)
 
 def cli_main():
     parser = setup_parser()
@@ -70,10 +78,13 @@ def cli_main():
     args = parser.parse_args()
 
     if args.command == 'visualize':
-        visualize(args.input, args.verbose)
+        visualize_from_file(args.input, args.verbose)
 
     if args.command == 'simulate':
-        simulate(args.output, args.verbose)
+        if args.show:
+            visualize(simulate(args.verbose), args.verbose)
+        else:
+            simulate_to_file(args.output, args.verbose)
 
 
 if __name__ == '__main__':
